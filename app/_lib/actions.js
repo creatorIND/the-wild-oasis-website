4981d43d1162a9eb068137646dcc14403941fe6b
Sujal Sinha <sujalsinha2243@gmail.com>
2025-02-02 04:44:03 +0530

    add server action to create bookings

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -57,6 +57,36 @@ export async function deleteReservation(bookingId) {
 	revalidatePath("/account/reservations");
 }
 
+export async function createBooking(bookingData, formData) {
+	const session = await auth();
+	if (!session) throw new Error("You must be logged in");
+
+	const newBooking = {
+		...bookingData,
+		guestId: session.user.guestId,
+		numGuests: Number(formData.get("numGuests")),
+		observations: formData.get("observations").slice(0, 1000),
+		extrasPrice: 0,
+		totalPrice: bookingData.cabinPrice,
+		isPaid: false,
+		hasBreakfast: false,
+		status: "unconfirmed",
+	};
+
+	const { data, error } = await supabase
+		.from("bookings")
+		.insert([newBooking])
+		.select("id")
+		.single();
+
+	if (error || !data) {
+		throw new Error("Booking could not be created");
+	}
+
+	revalidatePath(`/cabins/${bookingData.cabinId}`);
+	redirect(`/cabins/thankyou?bookingId=${data.id}`);
+}
+
 export async function updateBooking(formData) {
 	const session = await auth();
 	if (!session) throw new Error("You must be logged in");
