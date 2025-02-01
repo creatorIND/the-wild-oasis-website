58f543564df00ba7c6384931245d8cd29adbbe84
Sujal Sinha <sujalsinha2243@gmail.com>
2025-02-02 04:19:12 +0530

    add disabled prop and default values to all props

--- app/_components/SubmitButton.js
+++ app/_components/SubmitButton.js
@@ -2,13 +2,17 @@
 
 import { useFormStatus } from "react-dom";
 
-export default function SubmitButton({ children, pendingLabel }) {
+export default function SubmitButton({
+	children,
+	pendingLabel = "Submitting...",
+	disabled = false,
+}) {
 	const { pending } = useFormStatus();
 
 	return (
 		<button
 			className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
-			disabled={pending}>
+			disabled={pending || disabled}>
 			{pending ? pendingLabel : children}
 		</button>
 	);
